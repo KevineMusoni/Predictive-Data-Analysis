0bfc18721bc33e3dee7acd6a32a476f94286665a
RKatana <roduor41@gmail.com>
2019-01-31 20:16:19 +0300

    added the view for contacts and about page

--- dnalysis/views.py
+++ dnalysis/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    title ='Dnalysis-About us'
+    return render(request,'about.html',{'title':title})
+
+def contacts(request):
+    title='Dnalysis-Reach to us'
+    return render(request,'contacts.html',{'title':title})
