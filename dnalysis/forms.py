6329829571a9d6222c138f6c8fe1bcd0c1924ba1
Kevine Musoni <kevinenicky@gmail.com>
2019-02-02 22:02:06 +0300

    merged repo

---
+++ dnalysis/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Register,Space,Dailys
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = Register
+        fields = ('business_name','contact', 'email','description')
+
+class SpaceForm(forms.ModelForm):
+    class Meta:
+        model = Space
+        exclude =['register','posted_on']
+
+
+class DailysForm(forms.ModelForm):
+    class Meta:
+        model = Dailys
+        exclude =['register','posted_on']
--- dnalysis/views.py
+++ dnalysis/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .forms import RegisterForm,SpaceForm,DailysForm
 
 # Create your views here.
+def home_page(request):
+    
+    return render(request,'home.html',locals())
+
+def about(request):
+    title ='Dnalysis-About us'
+    return render(request,'about.html',{'title':title})
+
+def contacts(request):
+    title='Dnalysis-Reach to us'
+    return render(request,'contacts.html',{'title':title})
+
+def registration(request):
+    form = RegisterForm()
+    form1 = SpaceForm()
+    form2 = DailysForm()
+    return render(request, 'register/registration.html', {'form': form,'form1':form1,'form2':form2 })
