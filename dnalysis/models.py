d473a15d63430a76dd2e821bf86c859975e5dcf9
Kevine Musoni <kevinenicky@gmail.com>
2019-02-10 21:00:11 +0300

    merged code

--- dnalysis/models.py
+++ dnalysis/models.py
@@ -10,28 +10,55 @@ class Register(models.Model):
     """
     posted_on = models.DateTimeField(auto_now_add=True)
     business_name = models.CharField(max_length = 20)
-    email = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 50)
     contact = models.CharField(max_length = 30, blank=True)
-    description = models.TextField()
+    route = models.TextField()
 
 
+    def __str__(self):
+        return self.business_name
+
+    def save_editor(self):
+        self.save()
 
 class Space(models.Model):
     """
     Class that contains space for analysis, to be collected.
     """
-    minibuses = models.CharField(max_length = 20)
-    minibuses = models.CharField(max_length = 20)
-    matatus = models.CharField(max_length = 20)
+    minibuses = models.PositiveIntegerField()
+    minibuses = models.PositiveIntegerField()
+    matatus = models.PositiveIntegerField()
     posted_on = models.DateTimeField(auto_now_add=True)
     register = models.ForeignKey(Register, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.register
+
+    def save_editor(self):
+        self.save()
 
 class Dailys(models.Model):
-    """
-    Class that contains Daily data to be collected.
-    """
-    passengers = models.CharField(max_length = 20)
-    trips = models.CharField(max_length = 20)
-    income = models.CharField(max_length = 20)
-    posted_on = models.DateTimeField(auto_now_add=True)
-    register = models.ForeignKey(Register, on_delete=models.CASCADE)
+    MONTHS_OF_YEAR = (
+        ('January', 'January'),
+        ('February', 'February'),
+        ('March', 'March'),
+        ('April', 'April'),
+        ('May', 'May'),
+        ('June', 'June'),
+        ('July', 'July'),
+        ('August', 'August'),
+        ('September', 'September'),
+        ('October', 'October'),
+        ('November', 'November'),
+        ('December', 'December'),
+    )
+    month = models.CharField(max_length=60,choices=MONTHS_OF_YEAR)
+    no_of_passengers = models.PositiveIntegerField()
+    no_of_trips = models.PositiveIntegerField()
+    income = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.month
+
+    def save_editor(self):
+        self.save()
