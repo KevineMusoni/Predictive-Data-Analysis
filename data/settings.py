9f39c81788c9259571111d9ab77e998f046a2273
Ghost Rider <dianamulungi@gmail.com>
2019-02-12 11:48:34 +0300

    edit  settings

--- data/settings.py
+++ data/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dnalysis',
     'bootstrap4',
+    'dnalysis',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
